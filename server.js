b4b8f7c9f059145bf68b42a7e32213a371049d0e
ansaldiaros <dansaldi@ucclog.com>
2024-03-07 14:44:29 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const express =  require("express");
+const app = express();
+
+const PORT = 3000;
+// const PORT = 8000;
+
+app.listen(PORT,() => console.log("Express Listening PORT : ", + PORT));
+
+app.listen(console.log("hola mundo"));
+
